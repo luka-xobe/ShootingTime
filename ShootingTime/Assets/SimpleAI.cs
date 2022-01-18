0cafc478ff1450baf39312daf2a8c6850dd97240
Luka <khobeliya.luka@yandex.ru>
2022-01-18 23:53:40 +0300

    - Added waling ai

--- ShootingTime/Assets/SimpleAI.cs
+++ ShootingTime/Assets/SimpleAI.cs
@@ -1,36 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SimpleAI : MonoBehaviour
 {
-    private Rigidbody rb;
-    //rb = GetComponent<Rigidbody>();
+    public float lookRadius = 10f;
 
-
-    private void OnCollisionEnter(Collision collision)
+    Transform target;
+    NavMeshAgent agent;
+   
+    void Start()
     {
-        if (collision.gameObject.tag == "Player")
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+ 
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
         {
-            //guard.GetComponent<Guard>().enabled = true;
-           // reset = 5;
-            //GetComponent<SphereCollider>().enabled = false;
+            agent.SetDestination(target.position);
+            Debug.Log("WORKI");
+            //if (distance <= agent.stoppingDistance)
+            //{
+                // attack the target
+                // face the target 
+           // }
         }
     }
 
 
 
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnDrawGizmosSelected()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, lookRadius) ;
     }
 }
