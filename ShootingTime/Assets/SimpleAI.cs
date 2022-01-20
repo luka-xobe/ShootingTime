454e4fb0559ccd75eae35137321d7246f9291bcd
Luka <khobeliya.luka@yandex.ru>
2022-01-21 00:51:54 +0300

    - Small addition to AI

--- ShootingTime/Assets/SimpleAI.cs
+++ ShootingTime/Assets/SimpleAI.cs
@@ -8,18 +8,21 @@ public class SimpleAI : MonoBehaviour
     public float lookRadius = 10f;
 
     Transform target;
+    Animator animator;
     NavMeshAgent agent;
    
     void Start()
     {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
  
     void Update()
     {
         float distance = Vector3.Distance(target.position, transform.position);
+       
 
         if (distance <= lookRadius)
         {
@@ -27,10 +30,30 @@ public class SimpleAI : MonoBehaviour
             Debug.Log("WORKI");
             //if (distance <= agent.stoppingDistance)
             //{
-                // attack the target
-                // face the target 
-           // }
+            // attack the target
+            // face the target 
+            // }
+
+            animator.SetBool("Idle", false);
+            animator.SetBool("Move", true);
+
+            if (distance <= 5f)
+            {
+                animator.SetBool("Move", false);
+                animator.SetBool("Idle", true);
+                animator.SetBool("Fire", true);
+                Debug.Log("SHOOOOTIn");
+            }
+           
+
+
         }
+        else 
+        {
+            animator.SetBool("Move", false);
+            animator.SetBool("Idle", true);
+        }
+        
     }
 
 
